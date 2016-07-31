d4c859c6318c4b50404df33143236b96904f518c
William <xf0718@gmail.com>
2016-07-31 13:27:18 +0800

    delete rundanry

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import base64
 
 from bottle import route, get, post, put, delete
 from bottle import request, response, error
-from bottle import run, abort
-from bottle import redirect
+from bottle import redirect, abort
 from bottle import static_file
+from bottle import run
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -16,17 +16,6 @@ env = Environment(loader=FileSystemLoader('views'))
 @route('/static/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='public')
-
-@route('/template/a.html')
-def a_html():
-    template = env.get_template('a.html')
-    return template.render(action='hate')
-
-@route('/dashboard')
-def show_dashboard():
-    return 'haha'
-	# template = env.get_template('dashboard.html')
-	# return template.render()	
 
 @route('/login')
 @route('/')
@@ -39,6 +28,10 @@ def do_login():
     username = request.forms['username']
     password = request.forms['password']
     redirect('/dashboard')
+
+@route('/dashboard')
+def show_dashboard():
+    return 'This page is still under development'
 
 if __name__ == '__main__':
     run(host='localhost', port=5000, debug=True)
