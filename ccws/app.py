46ccc0ee9134386bc014222527533dfe68b5e059
William <xf0718@gmail.com>
2016-08-04 10:09:05 +0800

    copy views from ccws1

--- ccws/app.py
+++ ccws/app.py
@@ -11,11 +11,11 @@ from bottle import run
 
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('views'))
+env = Environment(loader=FileSystemLoader('ccws/views'))
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='public')
+    return static_file(filepath, root='ccws/public')
 
 @route('/login')
 @route('/')
@@ -29,10 +29,24 @@ def do_login():
     password = request.forms['password']
     redirect('/dashboard')
 
+dashboard_nav = (
+    ('users', '用户管理'),
+    ('informations', '情报管理'),
+    ('documents', '文档管理'),
+    ('regions', '区域管理'),
+)
+
 @route('/dashboard')
 def show_dashboard():
-    template = env.get_template('dashboard.html')
-    return template.render()
+    redirect('/dashboard/users')
+
+@route('/dashboard/<subsystem>')
+def show_dashboard_subsystem(subsystem):
+    if subsystem not in ('users', 'informations', 'documents', 'regions'):
+        abort(404)
+    template_file = '{}.html'.format(subsystem)
+    template = env.get_template(template_file)
+    return template.render(nav=dashboard_nav, subsystem=subsystem)
 
 if __name__ == '__main__':
     run(host='localhost', port=5000, debug=True)
